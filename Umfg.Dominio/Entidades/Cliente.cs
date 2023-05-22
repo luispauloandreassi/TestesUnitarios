091933e2c058cbcaa1ec9ef6feba4f967eaf726b
Luis Paulo <luis.andreassi@gmail.com>
2023-05-21 23:13:14 -0300

    Commit1

---
+++ Umfg.Dominio/Entidades/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umfg.Dominio.Entidades
+{
+    public sealed class Cliente
+    {
+        public string Documento { get; private set; }
+        public string Nome { get; private set; }
+
+        public Cliente(string documento, string nome)
+        {
+            Documento = documento 
+                ?? throw new ArgumentNullException(nameof(documento));
+            Nome = nome
+                ?? throw new ArgumentNullException(nameof(nome));
+        }
+    }
+}
